bcbfd682cfc72c11cb80ca9a3e251e40aa9f165a
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-11-02 06:16:21 +0100

    Exercise 82

---
+++ 15_Dekorator_@classmethod/exercise_82.py
@@ -0,0 +1,17 @@
+class Person:
+    instances = []
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        Person.instances.append(self)
+
+    @classmethod
+    def count_instances(cls):
+        return len(Person.instances)
+
+
+p1 = Person(first_name='Romek', last_name='Tomek')
+p2 = Person(first_name='Cezary', last_name='Cezary')
+
+print(p2.count_instances())
