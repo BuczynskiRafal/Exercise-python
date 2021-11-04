5174dae88612626e8ae6c9e4d39fe44ed29a4843
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-11-04 10:42:58 +0100

    Exercise 67

---
+++ 13_Hermetyzacja i enkapsulacja/exercise_68.py
@@ -0,0 +1,28 @@
+class Pet:
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+
+pet = Pet('Max', 5)
+print(pet.__dict__)
+pet.name = 'Tom'
+pet.age = 8
+print(pet.__dict__)
