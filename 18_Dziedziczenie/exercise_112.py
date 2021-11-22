e85f30b1ec85ebfb46fa0d56badc4e1261a752ab
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-11-22 12:22:36 +0100

    Dziedziczenie

---
+++ 18_Dziedziczenie/exercise_112.py
@@ -0,0 +1,20 @@
+class Vehicle:
+
+    def __init__(self, brand, color, year):
+        self.brand = brand
+        self.color = color
+        self.year = year
+
+
+class Car(Vehicle):
+
+    def __init__(self, brand, color, year, horsepower):
+        super().__init__(brand, color, year)
+        self.horsepower = horsepower
+
+
+vehicle = Vehicle('Tesla', 'red', 2020)
+print(vehicle.__dict__)
+
+car = Car('Tesla', 'red', 2020, 300)
+print(car.__dict__)
