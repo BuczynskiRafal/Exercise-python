a67f599e275234e13b8e2bcdc49af0140901da93
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-12-26 05:38:46 +0100

    CustomDate

--- 20_Podsumowanie/exercise_001.py
+++ 20_Podsumowanie/exercise_001.py
@@ -40,3 +40,58 @@ class Library:
 
     def show_books(self):
         print(self._collection)
+
+
+class CustomDate:
+
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self):
+        if self.day < 10:
+            if self.month < 10:
+                return f"0{self.day}-0{self.month}-{self.year}"
+            return f"0{self.day}-{self.month}-{self.year}"
+        if self.month < 10:
+            return f"{self.day}-0{self.month}-{self.year}"
+
+    @classmethod
+    def date_form_txt(cls, text):
+        day, mont, year = text.split("-")
+        return cls(int(day), int(mont), int(year))
+
+
+    @classmethod
+    def 
+
+
+
+d1 = CustomDate(7, 8, 2000)
+d2 = CustomDate(12, 8, 2001)
+d3 = CustomDate.date_form_txt("14-09-2002")
+print(d1)
+print(d2)
+print(d3)
+
+    # try:
+    #     datetime.datetime.strptime(dataod, "%Y-%m-%d")
+    # except ValueError:
+    #     raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
