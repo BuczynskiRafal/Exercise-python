21ceee30d821c7a5e54c55fa8982a08347b83f69
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-02-21 05:50:46 +0100

    07 stopień grafu przykład

--- 23 algorytmy - grafy/07 stopień grafu przykład.py
+++ 23 algorytmy - grafy/07 stopień grafu przykład.py
@@ -62,11 +62,33 @@ with open(FLIGHT_FILE, 'r') as csv_file:
                 if destination not in network[origin]:
                     network[origin].append(destination)
 
+
+
+# 7. No to mamy już wczytaną zawartość grafu! Jak chcesz możesz go wyświetlić (taaa na pewno
+# wszystko się zmieści na ekranie), albo chociaż wyświetlić informację o liczbie przetworzonych
+# wierszy:
 print(f'Processed {line_count} lines.')
 
+# 8. Dla wygody przetwarzania danych utwórz listę out_degrees, która będzie przechowywać nazwę
+# miasta/lotniska oraz liczbę wychodzących z tego lotniska połączeń. Elementami tej listy może
+# być słownik o dwóch kluczach: „name” określającym nazwę lotniska i „connections”
+# przechowującym liczbę miast/lotnisk do których można wylecieć z tego lotniska.
+out_degrees = [{'name': origin, 'connections': len(destinations) } for origin, destinations in network.items()]
 
-out_degrees = []
-for origin, destinations in network.items():
-    out_degrees.append({ 'name': origin, 'connections' : len(destinations) })
 
-max_airport =
+
+
+# 9. Wariant prostszy – znajdź miasto/lotnisko z największą liczbą połączeń – to na tyle proste, że nie
+# umieszczam dalszych instrukcji ;)
+# 10. Wariant trudniejszy – poszukamy 10 najbardziej skomunikowanych lotnisk. Korzystając z funkcji
+# sorted posortuj listę słowników out_degrees w kolejności malejącej (parametr reverse), ale
+# uwaga… sortowanie ma się odbyć po wartości słownika wskazywanej przez „connections”, ale
+# sortowane mają być całe słowniki z listy… I tutaj z pomocą przychodzi argument key, który
+# powinien wskazywać na itemgetter('connections'). Wynik zapisz w top10:
+max_degrees = max([airport['connections'] for airport in out_degrees])
+max_airport = [airport['name'] for airport in out_degrees if airport['connections'] == max_degrees]
+
+# 11. Z top10 wyświetl 10 pierwszych elementów:
+top_10 = sorted(out_degrees, key=itemgetter('connections'), reverse=True)[:10]
+for item in top_10:
+    print(item)
