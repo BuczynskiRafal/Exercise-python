ae1d6914ee90e0f2efead68197776ea563ae79a6
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-07-15 06:08:47 +0200

    exercise 26

--- 22_advanced/exercise_26.py
+++ 22_advanced/exercise_26.py
@@ -1,7 +1,11 @@
+import itertools
+
+
 def clean_hashtags():
     with open('hashtags.txt', 'r') as file:
         text = file.readlines()
-        print(text)
+    return sorted(list(set([word.replace('/n', '') for sublist in text for word in sublist.split() if len(word) <= 5])))
+
 
 
 print(clean_hashtags())
